a41a996c4ee02b4b928bf44f79650f72d532af9f
xm1989 <2316957100@qq.com>
2018-01-24 14:57:27 +0800

    11

---
+++ DYZB/DYZB/Classes/Tools/Common.swift
@@ -0,0 +1,14 @@
+//
+//  Common.swift
+//  DYZB
+//
+//  Created by 然博惠 on 2018/1/24.
+//  Copyright © 2018年 lm. All rights reserved.
+//
+
+import UIKit
+
+let kStatusBarH : CGFloat = 20
+let kNavigationBarH : CGFloat = 44
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
